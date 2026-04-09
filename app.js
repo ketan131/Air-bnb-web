1c4baa9c53c7e3e0b77ed678383c87ae1d3d7b1d
ketan131 <your-email@example.com>
2026-04-09 21:28:31 +0530

    here problem

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ passport.deserializeUser(user.deserializeUser());
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success"); 
     res.locals.error= req.flash("error");
-    res.locals.currentUser = req.user;
+    res.locals.currUser = req.user;
     next();
 });
 
