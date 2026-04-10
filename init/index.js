8536d3a02bdffea8908bbe67cb1de9f2cf28029b
ketan131 <your-email@example.com>
2026-04-10 10:22:56 +0530

    fix error

--- init/index.js
+++ init/index.js
@@ -17,7 +17,7 @@ async function  main(){
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
+    //await Listing.deleteMany({});
     initData.data = initData.data.map((obj) => ( {
         ...obj,
       owner: '68cd4e80d090427a2f2b1ee6'
@@ -27,4 +27,4 @@ const initDB = async () => {
     console.log("data was initialized");
 };
 
-initDB();
+//initDB();
